975a4a375218725355a3e513f48678cb9da062cb
ggamo987 <gemedran@cougarnet.uh.edu>
2024-02-17 20:41:00 -0600

    almost done

--- HW1-Medrano.py
+++ HW1-Medrano.py
@@ -1,8 +1,7 @@
 import re
-
+import random
 def problem1():
     cipher_text = "ROYQWH KQXXJYQ: N LQGNQAQ HDJH FO. VW NX J KQKLQO VZ J XQMOQH MONKQ VOYJWNSJHNVW MJGGQF U.D.J.W.H.V.K., IDVXQ YVJG NX HVHJG IVOGF FVKNWJHNVW. HDQNO UGJW NX HV JMBRNOQ J XRUQOIQJUVW JWF HV DVGF HDQ IVOGF OJWXVK. N JK JZOJNF HDJH IQ FV WVH DJAQ KRMD HNKQ LQZVOQ HDQT XRMMQQF.\nN DJAQ OQMQWHGT NWHQOMQUHQF JW QWMOTUHQF KQXXJYQ (JHHJMDKQWH MNUDQO2.HCH) HDJH IJX XQWH LT FO. VW HV VWQ VZ DNX MVWXUNOJHVOX, HDQ NWZJKVRX KO. LGVIZNQGF. N KJWJYQF HV FNXMVAQO HDJH HDQ KQXXJYQ IJX QWMOTUHQF RXNWY HDQ PJMEJG MNUDQO (XQQ XVROMQ MVFQ), LRH N IJX WVH JLGQ FNXMVAQO HDQ XQMOQH EQT, JWF HDQ MNUDQO XQQKX HV LQ RWLOQJEJLGQ. N JK JZOJNF HDJH FQMOTUHNWY HDNX KQXXJYQ NX HDQ VWGT IJT HV XHVU FO. VW'X VOYJWNSJHNVW.\nUGQJXQ XQWF OQNWZVOMQKQWHX NKKQFNJHQGT! N HONQF HV JMH MJRHNVRXGT, LRH N DJAQ J ZQQGNWY HDJH FO. VW'X DQWMDKQW JOQ VWHV KQ. N FVW'H EWVI DVI GVWY N DJAQ LQZVOQ HDQT FNXMVAQO KT OQJG NFQWHNHT JWF KT XQMOQH DNFNWY UGJ"
-    # BEGIN SOLUTION
     probabilities = {
         'E': '',
         'T': '',
@@ -40,8 +39,7 @@ def problem1():
             freq[char] += 1 
         else:
             freq[char] = 1
-    
-    #sort by the length of the value
+
     freq = dict(sorted(freq.items(), key=lambda item: item[1], reverse=True))
     stack = list(probabilities.keys())
     for char in freq:
@@ -55,8 +53,6 @@ def problem1():
         if cipher_text[char] in probabilities:
             cipher_text = cipher_text[:char] + probabilities[cipher_text[char]] + cipher_text[char+1:]
     print(cipher_text)
-    # END SOLUTION 
-    
 
 
 def JACKAL_Decrypt(firstKeyByte, secondKeyByte, cipherText):
@@ -80,10 +76,9 @@ def isEnglishText(byte):
         return False
     return True
 
-def Problem2():
-    with open("HW1_CyberSecurity/cipher2.txt", "rb") as file:
+def problem2():
+    with open("cipher2.txt", "rb") as file:
         cipherText = file.read()
-    # BEGIN SOLUTION
     plainText = ''
     for i in range(256):
         for j in range(256):
@@ -92,45 +87,27 @@ def Problem2():
                 break
         if isEnglishText(plainText):
             break
-    
-    # END SOLUTION
+
     print(plainText.decode())
 
 
 def problem3():
-    with open("HW1_CyberSecurity/cipher3.txt", "rb") as file:
+    with open("cipher3.txt", "rb") as file:
         cipher_text = file.read()
-    # BEGIN SOLUTION   
-    '''
-    AŌer decrypƟng the message, you immediately fly to Hawaii and prepare to intercept the
-exchange. With the element of surprise on your side, you easily defeat the agents of
-P.H.A.N.T.O.M. You expect to retrieve the secret plans from them; unfortunately, all you find is
-a USB drive with a single encrypted file.
-You have everything you need for decrypƟng cipher3.txt: the cipher algorithm (“oneƟme” pad
-with repeaƟng key) and the secret key. Hint: the binary XOR operaƟon in Python can be
-performed using the ^ operator.
-The output is the decrypted message. 
-    
-    '''
-    key = b"JACKAL"
+
+    key_phrase = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
     plain_text = bytearray()
     for i in range(len(cipher_text)):
-        plain_text.append(cipher_text[i] ^ key[i % len(key)])
-
-    
+        plain_text.append(cipher_text[i] ^ key_phrase[i % 11])
 
 
-    
-
-
-    # END SOLUTION
     print(plain_text.decode('utf-8'))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("\n\nProblem 1 \n\n")
-    #problem1()
+    problem1()
     print("\n\nProblem 2 \n\n")
-    #Problem2()
+    problem2()
     print("\n\nProblem 3 \n\n")
     problem3()
