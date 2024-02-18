941f5a39cd39edcedad9a63bf7d30ac58b6cbdc0
ggamo987UH <gemedran@cougarnet.uh.edu>
2024-02-17 19:13:22 -0600

    part 1 and part 2

--- HW1-Medrano.py
+++ HW1-Medrano.py
@@ -3,14 +3,60 @@ import re
 def problem1():
     cipher_text = "ROYQWH KQXXJYQ: N LQGNQAQ HDJH FO. VW NX J KQKLQO VZ J XQMOQH MONKQ VOYJWNSJHNVW MJGGQF U.D.J.W.H.V.K., IDVXQ YVJG NX HVHJG IVOGF FVKNWJHNVW. HDQNO UGJW NX HV JMBRNOQ J XRUQOIQJUVW JWF HV DVGF HDQ IVOGF OJWXVK. N JK JZOJNF HDJH IQ FV WVH DJAQ KRMD HNKQ LQZVOQ HDQT XRMMQQF.\nN DJAQ OQMQWHGT NWHQOMQUHQF JW QWMOTUHQF KQXXJYQ (JHHJMDKQWH MNUDQO2.HCH) HDJH IJX XQWH LT FO. VW HV VWQ VZ DNX MVWXUNOJHVOX, HDQ NWZJKVRX KO. LGVIZNQGF. N KJWJYQF HV FNXMVAQO HDJH HDQ KQXXJYQ IJX QWMOTUHQF RXNWY HDQ PJMEJG MNUDQO (XQQ XVROMQ MVFQ), LRH N IJX WVH JLGQ FNXMVAQO HDQ XQMOQH EQT, JWF HDQ MNUDQO XQQKX HV LQ RWLOQJEJLGQ. N JK JZOJNF HDJH FQMOTUHNWY HDNX KQXXJYQ NX HDQ VWGT IJT HV XHVU FO. VW'X VOYJWNSJHNVW.\nUGQJXQ XQWF OQNWZVOMQKQWHX NKKQFNJHQGT! N HONQF HV JMH MJRHNVRXGT, LRH N DJAQ J ZQQGNWY HDJH FO. VW'X DQWMDKQW JOQ VWHV KQ. N FVW'H EWVI DVI GVWY N DJAQ LQZVOQ HDQT FNXMVAQO KT OQJG NFQWHNHT JWF KT XQMOQH DNFNWY UGJ"
     # BEGIN SOLUTION
+    probabilities = {
+        'E': '',
+        'T': '',
+        'A': '',
+        'O': '',
+        'I': '',
+        'N': '',
+        'R': '',
+        'S': '',
+        'H': '',
+        'D': '',
+        'C': '',
+        'M': '',
+        'L': '',
+        'P': '',
+        'Y': '',
+        'G': '',
+        'U': '',
+        'W': '',
+        'B': '',
+        'F': '',
+        'V': '',
+        'K': '',
+        'Z': '',
+        'J': '',
+        'Q': '',
+        'X': ''
+    }
+
     freq = {}
     for char in cipher_text:
+        if not char.isalpha():
+            continue
         if char in freq:
-            freq[char] += 1  
+            freq[char] += 1 
         else:
-            freq[char] = 1 
+            freq[char] = 1
+    
+    #sort by the length of the value
+    freq = dict(sorted(freq.items(), key=lambda item: item[1], reverse=True))
+    stack = list(probabilities.keys())
+    for char in freq:
+        if len(stack) == 0:
+            break
+        probabilities[stack.pop(0)] = char
+
+    probabilities = {v:k for k, v in probabilities.items()}
+
+    for char in range(len(cipher_text)):
+        if cipher_text[char] in probabilities:
+            cipher_text = cipher_text[:char] + probabilities[cipher_text[char]] + cipher_text[char+1:]
+    print(cipher_text)
     # END SOLUTION 
-    print(freq)
+    
 
 
 def JACKAL_Decrypt(firstKeyByte, secondKeyByte, cipherText):
@@ -35,19 +81,47 @@ def isEnglishText(byte):
     return True
 
 def Problem2():
-    with open("cipher2.txt", "rb") as file:
+    with open("HW1_CyberSecurity/cipher2.txt", "rb") as file:
         cipherText = file.read()
     # BEGIN SOLUTION
-    # plainText = ....
+    plainText = ''
+    for i in range(256):
+        for j in range(256):
+            plainText = JACKAL_Decrypt(i, j, cipherText)
+            if isEnglishText(plainText):
+                break
+        if isEnglishText(plainText):
+            break
     
     # END SOLUTION
     print(plainText.decode())
 
 
 def problem3():
-    with open("cipher3.txt", "rb") as file:
+    with open("HW1_CyberSecurity/cipher3.txt", "rb") as file:
         cipher_text = file.read()
-    # BEGIN SOLUTION
+    # BEGIN SOLUTION   
+    '''
+    AŌer decrypƟng the message, you immediately fly to Hawaii and prepare to intercept the
+exchange. With the element of surprise on your side, you easily defeat the agents of
+P.H.A.N.T.O.M. You expect to retrieve the secret plans from them; unfortunately, all you find is
+a USB drive with a single encrypted file.
+You have everything you need for decrypƟng cipher3.txt: the cipher algorithm (“oneƟme” pad
+with repeaƟng key) and the secret key. Hint: the binary XOR operaƟon in Python can be
+performed using the ^ operator.
+The output is the decrypted message. 
+    
+    '''
+    key = b"JACKAL"
+    plain_text = bytearray()
+    for i in range(len(cipher_text)):
+        plain_text.append(cipher_text[i] ^ key[i % len(key)])
+
+    
+
+
+    
+
 
     # END SOLUTION
     print(plain_text.decode('utf-8'))
@@ -55,8 +129,8 @@ def problem3():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("\n\nProblem 1 \n\n")
-    problem1()
-    print("\n\nProblem 2 \n\n")
     #problem1()
+    print("\n\nProblem 2 \n\n")
+    #Problem2()
     print("\n\nProblem 3 \n\n")
-    #problem3()
+    problem3()
